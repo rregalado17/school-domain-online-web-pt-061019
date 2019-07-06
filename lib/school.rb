e7af8019cdbe13ef386835ef6e99f6c323c9538f
Robinson Regalado <rregalado17@gmail.com>
2019-07-06 17:45:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @roster = {}
+  end
+
+def add_student(student, grade)
+  if @roster.has_key?(grade) #we are looking to see if roster has a key of grade
+    @roster[grade] << student #if so, shove the student name into the hash
+  else
+    @roster[grade] = [student] #else, make a grade => student hash
+ end
+end
+
+def grade(grade)
+  @roster[grade]
+end
+ 
+ 
+end 
